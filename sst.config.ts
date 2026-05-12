781986ccca4eabdd4b4599d283de2ccf6fc40c5f
AlistairMross <87852242+AlistairMRoss@users.noreply.github.com>
2026-05-12 19:56:45 +0200

    remove config

--- sst.config.ts
+++ sst.config.ts
@@ -7,25 +7,25 @@ export default $config({
       removal: input?.stage === 'live' ? 'remove' : 'remove',
       protect: ['live'].includes(input?.stage),
       home: 'aws'
-      ,providers: {
-        aws: {
-          profile: 'my_site',
-          region: 'af-south-1'
-        }
-      }
+      // ,providers: {
+      //   aws: {
+      //     profile: 'my_site',
+      //     region: 'af-south-1'
+      //   }
+      // }
     };
   },
   async run() {
-    const { Monitor } = await import("whatwentwrong");                                                                                                                                            
-                                                            
-    const alerts = new Monitor("Alerts", {                                                                                                                                                        
+    const { Monitor } = await import("whatwentwrong");
+
+    const alerts = new Monitor("Alerts", {
       email: "alistairmikeross@gmail.com",
-    });   
+    });
 
     await import('./infra/Dynamo')
     const { Api } = await import('./infra/Api')
 
-    alerts.watch([Api], {metric: 'both'})
+    alerts.watch([Api], { metric: 'both' })
 
     new sst.aws.StaticSite("web", {
       path: 'web',
